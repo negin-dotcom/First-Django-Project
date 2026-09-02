fdc73f2262df728f2e2e6845af56b48623768d00
negin hajiloo <hajiloo@Net.local>
2026-09-02 11:31:10 +0330

    add order_items to orders

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,15 +1,51 @@
+from django.db import transaction
+
 from rest_framework import serializers
 
 from .models import Order, OrderItem
-
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order 
-        fields = ["created_by", "status", "total_price",]
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ["order", "product", "quantity", "price",]
+        read_only_fields = ["price",]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order 
+        fields = ["created_by", "status", "total_price", "order_items",]
+        read_only_fields = ["total_price",]
+
+    @transaction.atomic 
+    def create(self, validated_data):
+        order_items_data = validated_data.pop("order_items")
+
+        order = Order.objects.create(
+            **validated_data,
+            total_price=0
+        )
+
+        total_price = 0
+
+        for order_item_data in order_items_data:
+            product = order_item_data["product"]
+            quantity = order_item_data["quantity"]
+            order_item_price = product.price
+
+            OrderItem.objects.create(
+                order=order,
+                product=product,
+                quantity=quantity,
+                price=order_item_price
+            )
+
+            total_price += order_item_price * quantity 
+
+        order.total_price = total_price
+        order.save()
+
+        return order
